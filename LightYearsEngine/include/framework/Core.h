88843b9508f422586d834d4989e87198c2cc3499
EpicNav <navjot222@hotmail.com>
2025-02-13 20:06:56 -0500

    Game Engine Work in Progress

---
+++ LightYearsEngine/include/framework/Core.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <map>
+#include <stdio.h>
+
+namespace ly
+{
+    template<typename T>
+    using TUniquePtr = std::unique_ptr<T>;
+
+    template<typename T>
+    using TSharedPtr = std::shared_ptr<T>;
+
+    template<typename T>
+    using TWeakPtr = std::weak_ptr<T>;
+
+    template<typename T>
+    using TArray = std::vector<T>;
+
+    template<typename keyType, typename valueType, typename Pr = std::less<keyType>>
+    using TMap = std::map<keyType, valueType, Pr>;
+
+    template<typename keyType, typename valueType, typename Hasher = std::hash<keyType>>
+    using TUMap = std::unordered_map<keyType, valueType, Hasher>;
+    
+#define LOG(M, ...) printf(M "\n", ##__VA_ARGS__)
+}
